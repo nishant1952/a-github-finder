b1d44f9819d9f2e9a8eeac4d0a9f6b88053f8b1c
nckey <nishant1952k@gmail.com>
2021-07-31 16:25:42 +0530

    Update About.js
    minor style changes

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -3,18 +3,20 @@ import React, { Fragment } from 'react';
 export const About = () => {
 	return (
 		<Fragment>
-			<h1>About This App</h1>
-			<h3 className='my-3'>
-				React app to search Github profiles. This app uses the Context API along
-				with the useContext and useReducer hooks for state management.
-			</h3>
-			<h3>
-				Source Code hosted on -{' '}
-				<a href='https://github.com/nishant1952' className='underline'>
-					{' '}
-					<i className='fab fa-github'> Github </i>
-				</a>
-			</h3>
+			<div className='my-2'>
+				<h1>About This App</h1>
+				<h2 className='my-3'>
+					React app to search Github profiles. This app uses the Context API
+					along with the useContext and useReducer hooks for state management.
+				</h2>
+				<h3>
+					Source Code hosted on -{' '}
+					<a href='https://github.com/nishant1952' className='underline'>
+						{' '}
+						<i className='fab fa-github'> Github </i>
+					</a>
+				</h3>
+			</div>
 		</Fragment>
 	);
 };
